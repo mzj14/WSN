a003025058f36677e8181675b8e43585c76608f6
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-22 19:46:14 +0800

    add simple application to print data to console, will be used to generate result.txt

--- PC_File/Oscilloscope.java
+++ PC_File/Oscilloscope.java
@@ -36,31 +36,18 @@ import java.io.*;
 public class Oscilloscope implements MessageListener
 {
     MoteIF mote;
-    Data data;
-    Window window;
-
-    /* The current sampling period. If we receive a message from a mote
-       with a newer version, we update our interval. If we receive a message
-       with an older version, we broadcast a message with the current interval
-       and version. If the user changes the interval, we increment the
-       version and broadcast the new interval and version. */
-    int interval = Constants.DEFAULT_INTERVAL;
-    int version = -1;
-
-    /* Main entry point */
+    //
+    // /* The current sampling period. If we receive a message from a mote
+    //    with a newer version, we update our interval. If we receive a message
+    //    with an older version, we broadcast a message with the current interval
+    //    and version. If the user changes the interval, we increment the
+    //    version and broadcast the new interval and version. */
+    //
+    // /* Main entry point */
     void run() {
-    System.out.println("PC File application run");
-    data = new Data(this);
-    window = new Window(this);
-    window.setup();
-    mote = new MoteIF(PrintStreamMessenger.err);
-    mote.registerListener(new OscilloscopeMsg(), this);
-    }
-
-    /* The data object has informed us that nodeId is a previously unknown
-       mote. Update the GUI. */
-    void newNode(int nodeId) {
-    window.newNode(nodeId);
+       System.out.println("PC File application run");
+       mote = new MoteIF(PrintStreamMessenger.err);
+       mote.registerListener(new OscilloscopeMsg(), this);
     }
 
     void outputMsg(OscilloscopeMsg omsg) {
@@ -73,7 +60,6 @@ public class Oscilloscope implements MessageListener
         System.out.print("light = " + omsg.get_light());
         System.out.print("current_time = " + omsg.get_current_time());
         System.out.print("token = " + omsg.get_token());
-        // System.out.print("readings = " + omsg.get_readings());
         System.out.print("\n");
     }
 
@@ -83,67 +69,13 @@ public class Oscilloscope implements MessageListener
         OscilloscopeMsg omsg = (OscilloscopeMsg)msg;
         // System.out.println("token = " + omsg.get_token());
         // if (omsg.get_token() == 101) {
-            outputMsg(omsg);
-        // }
+        outputMsg(omsg);
 
-        /* Update interval and mote data */
-        periodUpdate(omsg.get_version(), omsg.get_interval());
-        // data.update(omsg.get_id(), omsg.get_count(), omsg.get_readings());
-
-        /* Inform the GUI that new data showed up */
-        // window.newData();
     }
     }
-
-    /* A potentially new version and interval has been received from the
-       mote */
-    void periodUpdate(int moteVersion, int moteInterval) {
-    if (moteVersion > version) {
-        /* It's new. Update our vision of the interval. */
-        version = moteVersion;
-        interval = moteInterval;
-        window.updateSamplePeriod();
-    }
-    else if (moteVersion < version) {
-        /* It's old. Update the mote's vision of the interval. */
-        sendInterval();
-    }
-    }
-
-    /* The user wants to set the interval to newPeriod. Refuse bogus values
-       and return false, or accept the change, broadcast it, and return
-       true */
-    synchronized boolean setInterval(int newPeriod) {
-    if (newPeriod < 1 || newPeriod > 65535) {
-        return false;
-    }
-    interval = newPeriod;
-    version++;
-    sendInterval();
-    return true;
-    }
-
-    /* Broadcast a version+interval message. */
-    void sendInterval() {
-    OscilloscopeMsg omsg = new OscilloscopeMsg();
-
-    omsg.set_version(version);
-    omsg.set_interval(interval);
-    try {
-        mote.send(MoteIF.TOS_BCAST_ADDR, omsg);
-    }
-    catch (IOException e) {
-        window.error("Cannot send message to mote");
-    }
-    }
-
-    /* User wants to clear all data. */
-    void clear() {
-    data = new Data(this);
-    }
-
     public static void main(String[] args) {
     Oscilloscope me = new Oscilloscope();
     me.run();
     }
+
 }
